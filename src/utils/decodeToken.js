b40a92aab3242c5eaec6f17273bacc55e957c82b
Zoran Dasic <zorandsc@yahoo.com>
2024-04-11 11:23:26 +0200

    test1

--- src/utils/decodeToken.js
+++ src/utils/decodeToken.js
@@ -1,18 +1,15 @@
 import jwt_decode from "jwt-decode";
 
 const decodeToken = (accessToken) => {
-  const decoded = accessToken ? jwt_decode(accessToken) : undefined;
+  let username = "";
+  let roles = [];
 
-  //ovo kada je na backend npm i jesonwebtoken
-  //const decode=jwt_decode(accessToken)
-  //const roles = decode?.UserInfo?.roles || [];
-  //const username = decoded?.UserInfo?.username;
-
-  //a ovo kada je na backend npm i jose
-  const roles = decoded ? JSON.parse(decoded?.sub)?.UserInfo?.roles || [] : [];
-  const username = decoded
-    ? JSON.parse(decoded?.sub)?.UserInfo?.username || ""
-    : "";
+  let decoded = accessToken ? jwt_decode(accessToken) : undefined;
+  console.log("form decodetoken", decoded)
+  if (decoded) {
+    roles = JSON.parse(decoded?.sub)?.UserInfo?.roles;
+    username = JSON.parse(decoded?.sub)?.UserInfo?.username;
+  }
 
   return { username, roles };
 };
